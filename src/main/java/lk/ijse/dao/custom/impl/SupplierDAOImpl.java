b8faabd8483e92063adb11a64e0e842d1f3d4cf1
lahiru_lolitha <lahiru212001@gmail.com>
2024-01-10 19:41:34 +0530

    Supplier supplier DAO connect SQL Util

--- src/main/java/lk/ijse/dao/custom/impl/SupplierDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/SupplierDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.dao.SQLUtil;
 import lk.ijse.dao.custom.SupplierDAO;
 import lk.ijse.db.DbConnection;
 import lk.ijse.dto.SupplierDto;
@@ -13,14 +14,9 @@ import java.util.ArrayList;
 public class SupplierDAOImpl implements SupplierDAO {
     @Override
     public ArrayList<SupplierDto> getAll() throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
-
         String sql = "SELECT * FROM supplier ";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-
+        ResultSet resultSet = SQLUtil.execute(sql);
         ArrayList<SupplierDto> supplierDtoList = new ArrayList<>();
-
-        ResultSet resultSet = pstm.executeQuery();
         while (resultSet.next()) {
             String Supplier_id = resultSet.getString(1);
             String Supplier_name = resultSet.getString(2);
@@ -37,27 +33,21 @@ public class SupplierDAOImpl implements SupplierDAO {
 
     @Override
     public boolean save(SupplierDto dto) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
         String sql = "INSERT INTO supplier VALUES (?, ?, ?, ?, ?)";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1, dto.getSupplierId());
-        pstm.setString(2, dto.getSupplierName());
-        pstm.setString(3, dto.getSupplierNIC());
-        pstm.setString(4, dto.getSupplierAddress());
-        pstm.setString(5, dto.getSupplierContactNumber());
+        return  SQLUtil.execute(sql,
+                dto.getSupplierId(),
+                dto.getSupplierName(),
+                dto.getSupplierNIC(),
+                dto.getSupplierAddress(),
+                dto.getSupplierContactNumber()
+                );
 
-        boolean isSvaed = pstm.executeUpdate() > 0;
-
-        return isSvaed;
     }
 
     @Override
     public SupplierDto search(String searchSupplierIDText) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
         String sql = "SELECT * FROM supplier WHERE supplier_id= ?";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1, searchSupplierIDText);
-        ResultSet resultSet = pstm.executeQuery();
+        ResultSet resultSet = SQLUtil.execute(sql,searchSupplierIDText);
         SupplierDto dto = null;
         if (resultSet.next()) {
             dto = new SupplierDto(
@@ -68,34 +58,25 @@ public class SupplierDAOImpl implements SupplierDAO {
                     resultSet.getString("contact_number")
             );
         }
-
         return dto;
     }
 
     @Override
     public boolean update(SupplierDto dto) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
         String sql = "UPDATE supplier SET supplier_name=?, NIC=?, address=?, contact_number=? WHERE supplier_id=?";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1, dto.getSupplierName());
-        pstm.setString(2, dto.getSupplierNIC());
-        pstm.setString(3, dto.getSupplierAddress());
-        pstm.setString(4, dto.getSupplierContactNumber());
-        pstm.setString(5, dto.getSupplierId());
-
-        boolean isUpdated = pstm.executeUpdate() > 0;
-        return isUpdated;
+      return  SQLUtil.execute(sql,
+                dto.getSupplierName(),
+                dto.getSupplierNIC(),
+                dto.getSupplierAddress(),
+                dto.getSupplierContactNumber(),
+                dto.getSupplierId()
+                );
     }
 
     @Override
     public boolean delete(String supplierId) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
         String sql = "DELETE FROM supplier WHERE supplier_id=?";
-
-        PreparedStatement pstm = connection.prepareStatement(sql);
-        pstm.setString(1, supplierId);
-        boolean isDeleted = pstm.executeUpdate() > 0;
-        return isDeleted;
+        return SQLUtil.execute(sql);
     }
 
     @Override
