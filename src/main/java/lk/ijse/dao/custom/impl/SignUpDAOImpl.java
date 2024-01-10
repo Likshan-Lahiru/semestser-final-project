2e5c03ca86e3116935558b2535fcecab2426f700
lahiru_lolitha <lahiru212001@gmail.com>
2024-01-10 19:25:07 +0530

    sign up get name function connect  SQL Util class

--- src/main/java/lk/ijse/dao/custom/impl/SignUpDAOImpl.java
+++ src/main/java/lk/ijse/dao/custom/impl/SignUpDAOImpl.java
@@ -1,5 +1,6 @@
 package lk.ijse.dao.custom.impl;
 
+import lk.ijse.dao.SQLUtil;
 import lk.ijse.dao.custom.SignUpDAO;
 import lk.ijse.db.DbConnection;
 import lk.ijse.dto.SignUpDto;
@@ -10,17 +11,13 @@ import java.sql.SQLException;
 public class SignUpDAOImpl implements SignUpDAO {
     @Override
     public boolean createAccount(SignUpDto dto) throws SQLException {
-        Connection connection = DbConnection.getInstance().getConnection();
         String sql = "INSERT INTO user VALUES(?, ?, ?, ?, ?)";
-        PreparedStatement pstm = connection.prepareStatement(sql);
-
-        pstm.setString(1, dto.getUserName());
-        pstm.setString(2, dto.getFirstName());
-        pstm.setString(3, dto.getScondName());
-        pstm.setString(4, dto.getEmail());
-        pstm.setString(5, dto.getPasssword());
-
-        boolean isCreateAccount = pstm.executeUpdate()>0;
-        return isCreateAccount;
+       return SQLUtil.execute(sql,
+                dto.getUserName(),
+                dto.getFirstName(),
+                dto.getScondName(),
+                dto.getEmail(),
+                dto.getPasssword()
+        );
     }
 }
